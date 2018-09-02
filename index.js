6742d8d5aa7e9f7573aa9d75085dee2752620306
devOpElliot <efortney@missouriwestern.edu>
2018-09-02 17:31:31 -0500

    RIDR-03_Uber_Oauth completed uber authorization route

--- index.js
+++ index.js
@@ -2,10 +2,36 @@
  * Index page
  *
  */
-
 const express = require('express');
 const https = require('https');
 const app = express();
+const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
+const Uber = './routes/Auth/Uber.js';
+const passport = require('passport');
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: ['asdfjasdviuasdaehr'],
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.json());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+require('./routes/auth/Uber')(app);
+require('./routes/api/routes')(app);
+require('./services/passport');
 
 const port = process.env.NODE_ENV || 8080;
 
---
+++ routes/api/routes.js
@@ -0,0 +1,11 @@
+const passport = require('passport');
+
+module.exports = app => {
+
+    // logout route 
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.redirect('/');
+      });
+
+}
