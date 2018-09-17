94676c724bbfe7163bb221a4c0cdad0fb85343c8
Elliot <efortney@missouriwestern.edu>
2018-09-17 16:52:16 -0500

    Ridr 01 create app header (#9)
    * RIDR-01 started app header
    
    * Added nav bar component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,26 @@
-// Help page
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 
-import '../styles/header.css';
+require('../styles/header.css');
 
-class Header extends Component {
-  render() {
-    return (
-      <h1> HEADER </h1>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <div className="black nav-wrapper">
+          <a href="/" className="brand-logo">
+           <span className="homeText logo">RIDR </span>
+          </a>
+          <ul className="right">
+            <li>
+              <a href="/profile">
+                <i className="material-icons">account_circle</i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Header;
---
+++ client/src/components/marketing.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const Marketing = () => {
+
+    return (
+        <div className='marketing'>
+            <div className="container">
+                <div className="row">
+                    
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Marketing;
