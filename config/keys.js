8c785caa3a765082b1dd0c312fa04d32a3c9de30
devOpElliot <efortney@missouriwestern.edu>
2020-03-04 19:36:39 -0600

    removng keys

--- config/keys.js
+++ config/keys.js
@@ -1,16 +1,6 @@
 /**
  * Configuration file containing development keys 
- * DO NOT COMMIT THIS TO GIT!!!!!!
  */
 
  module.exports = {
-     uberClientID: 'XDxhGf5gKETizrCvi6kLmfivLbatNJX_',
-     uberClientSecret: 'X96VwqLL_2XVx1FIG7eJqr46w9lB8pJ6_x8CxWIA',
-     lyftClientID: '1lddKeQDyRYh',
-     lyftSecret:'_sP9qoABnjVJhulZRCTmXuVR0CwyMWpI',
-     googleClientID: '562944288342-6sb1ksjmslimm7cnpvrggckmgm8au45c.apps.googleusercontent.com',
-     googleClientSecret: 'pT1bm-0k550emvgfGGCVd4e0',
-     mongo: 'mongodb://efortney227:elliot123@ds129393.mlab.com:29393/ridr',
-     googleMap: 'AIzaSyC6UtxcZVztpcSZGOrCtAWfHSCN6PzJtL4',
-     yelp : 'Wwl2uAr5ECJodlKe82OKdteOiMOpYVd6Sh937pCTz8GRjnsUu37vkw81Ky-GIzb_sOpacgKSWgypGC58h_C0oyx6OxrQntgFDjlO1KZw9iDTd6NqDZIYdy-JfSr_W3Yx'
  }
